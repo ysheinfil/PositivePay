701e2aa23ed8cc66e03188a1aff6dfb419ac0de6
Yossi Sheinfil <28785089+ysheinfil@users.noreply.github.com>
2020-04-30 12:36:06 -0400

    Add files via upload

---
+++ PositivePayCoreLibrary/IBankRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PositivePayCoreLibrary
+{
+    public interface IBankRecord
+    {
+        public string AccountNumber { get; set; }
+        public int CheckNumber { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Payee { get; set; }
+    }
+}
---
+++ PositivePayCoreLibrary/MTBank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PositivePayCoreLibrary
+{
+    public class MTBank : IBankRecord
+    {
+        public string AccountNumber { get; set; }
+        public int CheckNumber { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Payee { get; set; }
+    }
+}
